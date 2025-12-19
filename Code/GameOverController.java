1352fd4d882d7b42cfd320c2f7d6d2c9ed6d51cd
YeYint700 <yeyintaung.dec@gmail.com>
2025-12-19 10:22:41 +0900

    test
    test

---
+++ Code/GameOverController.java
@@ -0,0 +1,20 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GameOverController {
+
+    @FXML
+    void onGameOverAction(ActionEvent event) {
+        try {
+            StageDB.getGameOverStage().hide();
+            StageDB.getMainSound().stop();
+
+            StageDB.resetMainStage();
+            StageDB.getMainStage().show();
+            StageDB.getMainSound().play();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
