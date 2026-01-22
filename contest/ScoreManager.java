94822a52e94ed4d57d187aa8293f8028fe1c5d16
Soshi-Uematsu <uematsu.soshi.25@shizuoka.ac.jp>
2026-01-22 12:20:59 +0900

    ScoreManager.java

--- contest/ScoreManager.java
+++ contest/ScoreManager.java
@@ -5,17 +5,10 @@ public class ScoreManager {
     public static int getScore() {
         return Score;
     }
-    //残り時間に応じた得点
-    public static void scorebyTime(int remainingSeconds) {
-        int timeScore= remainingSeconds * 1000;
-    }
-    //アイテムの取得数に応じた得点
-    public static void scorebyItem(int itemCount) {
-        int itemScore= itemCount * 500;
-    }
+    
     //合計得点
     public static void totalScore(int remainingSeconds, int itemCount) {
-        Score = (remainingSeconds * 1000) + (itemCount * 500);
+        Score = (remainingSeconds * 100) + (itemCount * 300);
     }
     //得点のリセット
     public static void resetScore() {
